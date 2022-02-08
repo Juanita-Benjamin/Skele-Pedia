59f7ba33808b618c2d591880e5e7b67d286c1029
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2022-02-08 02:16:26 -0600

    Add files via upload

---
+++ Skele-Pedia/Assets/Scenes/Game 3 - Skele Trivia/TriviaGame.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriviaGame : MonoBehaviour
+{
+    public int i = 0; //index
+    public GameObject[] Bones;
+    //public int length;
+
+    public Button[] options = new Button[4];
+    public Text[] optionsText;
+
+    public GameObject correctAnswer;
+    public string[] choices = new string[4];
+
+    public Text QuestionText;
+    public int clicked;
+
+    void Start(){
+
+        correctAnswer = GetRandomCorrectAnswer(Bones);
+        correctAnswer.AddComponent<highLight>();
+        correctAnswer.GetComponent<highLight>().hover = false;
+        correctAnswer.GetComponent<highLight>().highLightThis();
+        Debug.Log(Bones[0].name);
+        choices[0] = correctAnswer.name;
+        Debug.Log(choices[0]);
+        int chosenBone = 0;
+        for(int i = 1; i<choices.Length;i++){
+            int randVal = Random.Range(0,Bones.Length);
+            if(Bones[randVal]==correctAnswer || Bones[randVal].name == "blank"){
+                randVal = (randVal + 1) % (Bones.Length);
+            }
+            
+            string randValStr = Bones[randVal].name;
+            choices[i] = randValStr;
+            Debug.Log(choices[i]);
+            Bones[randVal].name="blank";
+            
+        }
+        loadText();
+        //displayBone();
+    }
+
+
+    void loadText(){
+        for(int i=0; i<choices.Length;i++){
+            optionsText[i].text = choices[i];
+        }
+    }
+
+    void displayBone(){
+        GameObject boneOnDisplay = Instantiate(correctAnswer,transform.position, Quaternion.identity);
+        //boneOnDisplay.transform.localScale = new Vector3(5f,5f,5f); //make it twice as big as it normally would be
+    }
+
+    void displayCorrect(){
+        
+    }
+
+    public void LoadButtons(){
+        for(int i=0;i<options.Length;i++)
+        {
+            options[i].onClick.AddListener(() => OnButtonClick(i+1));
+        }
+    }
+
+    public void OnButtonClick(int id)
+    {
+        Debug.Log(id);
+        clicked = id;
+    }
+
+    public GameObject GetRandomCorrectAnswer(GameObject[] Bones){
+        return Bones[Random.Range(0,Bones.Length)];
+    }
+    
+    
+}
---
+++ Skele-Pedia/Assets/Scenes/Game 3 - Skele Trivia/readBones.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class readBones : MonoBehaviour
+{
+    //to not be seen
+    //reads all the bones in a premade skeleton gameObject and sends that list to the trivia game list
+    public GameObject skeleton;
+    public List<string> listOfBones = new List<string>();
+
+    public GameObject skull;
+    public string name;
+
+    void Start(){
+        name = skull.name;
+    }
+    void loadBones(){
+        //foreach (GameObject child in skeleton) {
+         //   listOfBones.Add(child.name); 
+        //}
+    }
+}
