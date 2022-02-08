5e1e565ca10ce4a735bc25ac72742bbd0b9ade4b
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2022-02-08 12:15:06 -0600

    Added Comments to Answers.cs

--- Skele-Pedia/Assets/Scenes/Game 3 - Skele Trivia/Answers.cs
+++ Skele-Pedia/Assets/Scenes/Game 3 - Skele Trivia/Answers.cs
@@ -5,6 +5,6 @@ using UnityEngine.UI;
 
 public class Answer : MonoBehaviour
 {
-    public bool isCorrect = false;
-    public string nameField;
+    public bool isCorrect = false; //we may not need this class anymore - I'm not sure
+    public string nameField; //its basically a string class with an added true false field
 }
