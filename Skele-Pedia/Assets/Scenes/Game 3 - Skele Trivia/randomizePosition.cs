538774eca2e3b5ace826bfec8789c5b9cb264846
SaifTTU <70453733+SaifTTU@users.noreply.github.com>
2022-03-06 08:39:26 -0600

    Add files via upload

---
+++ Skele-Pedia/Assets/Scenes/Game 3 - Skele Trivia/randomizePosition.cs
@@ -0,0 +1,63 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomizePosition : MonoBehaviour
+{
+    public GameObject[] allTheBones;
+    public Vector3[] allTheBonesPositions;
+    public Vector3[] originalPositions;
+    public Vector3[] correctPositions;
+
+    public GameObject temporaryGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTheBonesPositions = new Vector3[allTheBones.Length];
+        originalPositions = new Vector3[allTheBonesPositions.Length];
+        correctPositions = new Vector3[allTheBonesPositions.Length];
+
+        for(int i = 0; i < allTheBones.Length; i++){
+            allTheBonesPositions[i] = allTheBones[i].transform.position;
+            correctPositions[i] = allTheBonesPositions[i];
+        }
+        
+        Randomize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Randomize() {
+         for (int i = 0; i < allTheBones.Length; i++) {
+             int rnd = Random.Range(0, allTheBones.Length); //random number created 
+             temporaryGameObject = allTheBones[rnd]; //we set the currently,
+             allTheBones[rnd] = allTheBones[i];
+             allTheBones[i] = temporaryGameObject;
+         }
+         for(int i = 0; i < allTheBones.Length; i++){
+            allTheBones[i].transform.position = allTheBonesPositions[i];
+        }
+        saveCurrentSet();
+     }
+
+     public void saveCurrentSet(){
+         for(int i = 0; i < allTheBones.Length; i++){
+            originalPositions[i] = allTheBonesPositions[i];
+        }
+     }
+    public void resetAll(){
+        for(int i = 0; i < allTheBones.Length; i++){
+            allTheBones[i].transform.position = originalPositions[i];
+        }
+    }
+    public void makeCorrect(){
+        for(int i = 0; i < allTheBones.Length; i++){
+            allTheBones[i].transform.position = correctPositions[i];
+        }
+    }
+
+}
