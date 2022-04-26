2c0a7f0a901a238b832463cff6618052de1f75a0
tabithaurb <44306680+tabithaurb@users.noreply.github.com>
2022-04-26 15:25:37 -0500

    Update PlaceSkeleton.cs

--- Skele-Pedia/Assets/Scripts/PlaceSkeleton.cs
+++ Skele-Pedia/Assets/Scripts/PlaceSkeleton.cs
@@ -2,13 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Android;
-using UnityEngine.Serialization;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
-
+using  TMPro;
 public class PlaceSkeleton : MonoBehaviour
 {
+    public int score;
+
+    public TMP_Text scoretext;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +26,11 @@ public class PlaceSkeleton : MonoBehaviour
             Debug.Log(other.name +"will snap to:" + gameObject);
             other.gameObject.transform.localPosition = new Vector3(0, 0, 0);
             Destroy(other.GetComponent<Drag>());
-            
+
+            score += 10;
+            scoretext.text = score.ToString();
         }
+        
         
     }
 }
